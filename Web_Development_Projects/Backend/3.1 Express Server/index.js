2c6c0c0290ab78a5bf1535561b4dbfd42969f0aa
ifuta <ifuifu08@gmail.com>
2024-11-02 11:02:42 +0900

    Update Second

--- Web_Development_Projects/Backend/3.1 Express Server/index.js
+++ Web_Development_Projects/Backend/3.1 Express Server/index.js
@@ -2,6 +2,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('3.1');
+  // console.log(req.rawHeaders);
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
